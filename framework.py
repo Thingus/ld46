55dc1b99d31d9791a3ab970efd2fb4bed94a547d
Thingus <fletcherroberts049@gmail.com>
2020-04-18 19:18:33 +0100

    Mid-entity implementation

--- framework.py
+++ framework.py
@@ -1,8 +1,10 @@
 import pygame
 from random import randint
 import math
-
+from enum import Enum
 import pdb
+
+object_dict = {obj_id:None for obj_id in range(0, 300)}    # I know globals are bad but this is LD, baby.
 
 class PygView(object):
     """
@@ -20,13 +22,14 @@ class PygView(object):
         self.fps = fps
         self.playtime = 0.0
         self.font = pygame.font.SysFont("mono", 20, bold=True)
-
-        self.object_list = []
+        
+        door = Door(self.screen, 320, 480)
+        global object_dict
+        object_dict[door.id] = door
 
     def run(self):
         running = True
-        self.object_list.append(Partygoer(parent_object = self, x = 320, y=240))
-        self.object_list[0].set_move_target(0, 0, 2)
+        global object_dict
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -34,16 +37,24 @@ class PygView(object):
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         running = False
+                    if event.key == pygame.K_d:
+                        pdb.set_trace()
             milliseconds = self.clock.tick(self.fps)
             self.playtime += milliseconds /1000.0
             self.draw_text("FPS: {:6.3}\nPLAYTIME: {:6.3} SECONDS".format(
                            self.clock.get_fps(), self.playtime))
-            for updateable in self.object_list:
-                updateable.update()
+            for updateable in object_dict.values():
+                try:
+                    updateable.update()
+                except AttributeError:
+                    pass
             pygame.display.flip()
             self.screen.blit(self.surface, (0,0))
-            for drawable in self.object_list:
-                drawable.draw()
+            for drawable in object_dict.values():
+                try:
+                    drawable.draw()
+                except AttributeError:
+                    pass
         pygame.quit()
 
     def draw_text(self, text):
@@ -52,17 +63,72 @@ class PygView(object):
         self.screen.blit(surface, (0, 0)) 
 
 
+class Door(object):
+    """
+    Spawns partygoers
+    """
+    def __init__(self, parent_surface, x, y, spawn_rate=10, max_guests = 20, width=20, height=10):
+        self.surface = pygame.Surface((width, height))
+        self.x = x
+        self.y = y
+        self.spawn_rate = spawn_rate
+        self.parent_surface = parent_surface
+        self.max_guests = max_guests
+        self.spawn_ticker = 0
+        self.spawned_guests = 0
+        self.id = randint(50, 100)
+        
+    def new_guest(self):
+        Partygoer(self.parent_surface, randint(10, 600), randint(400, 480))
+        self.spawned_guests += 1
+
+    def draw(self):
+        pass
+    
+    def update(self):
+        if self.spawned_guests > self.max_guests:
+            return
+        self.spawn_ticker += 1
+        if self.spawn_ticker % self.spawn_rate == 0:
+            self.spawn_ticker = 0
+            self.new_guest()
+    
+    """
+    def guest_left(self, guest):
+        guest.
+    """ 
+        
+
+class Mood(Enum):
+    DANCE = 1
+    TALK = 2
+    LEAVE = 0
+
+class PartyState(Enum):
+    ENTERING = 0
+    MOVING_TO_DANCE = 1
+    MOVING_TO_TALK = 2
+    DANCING = 3
+    TALKING = 4
+    MOVING_TO_LEAVE = 5
+
 class Partygoer(object):
     
-    def __init__(self, parent_object, x, y,  width = 40, height=120):
+    def __init__(self, parent_surface, x, y,  width = 40, height=120, mood=None):
         self.width = width
         self.height = height
         self.surface = pygame.Surface((width, height))
-        self.parent_surface = parent_object.surface
+        self.parent_surface = parent_surface
         self.x = x
         self.y = y
         self.generate_sprite()
-        
+        self.id = randint(100, 300)
+        global object_dict
+        object_dict[self.id] = self
+        if not mood:
+            mood = Mood(randint(1,2))
+        self.state = PartyState.ENTERING
+
     def generate_sprite(self):
         skin_color = gen_random_color()
         tshirt_color = gen_random_color()
@@ -101,6 +167,68 @@ class Partygoer(object):
         self.surface.set_colorkey((255, 255, 255))
         self.surface.convert()
 
+    def state_update(self):
+        """
+        Check for state transitions
+        """
+        if self.state = PartyState.ENTERING:
+            if mood == Mood.DANCING:
+                self.state = PartyState.MOVING_TO_DANCE
+            elif mood == Mood.TALKNG:
+                self.state = PartyState.MOVING_TO_TALK
+        elif self.state = PartyState.MOVING_TO_DANCE:
+            if self.reached_target:
+                self.state = PartyState.DANCING
+        elif self.state = PartyState.MOVING_TO_TALK:
+            if self.reached_target:
+                self.state = PartyState.TALKING
+        elif self.state = PartyState.DANCING:
+            if mood = Mood.TALKING:
+                self.state = PartyState.MOVING_TO_TALK
+            elif mood = Mood.LEAVING
+                self.state = PartyState.MOVING_TO_LEAVE
+        elif self.state = PartyState.TALKING:
+            if mood = Mood.DANCING:
+                self.move_to_dance()
+            elif mood = Mood.LEAVING:
+                self.state = PartyState.MOVING_TO_LEAVE
+    
+    def move_to_dance(self):
+        # Pick random spot on dancefloor area
+        set_move_target(
+            x = randint(10, 300),
+            y = randint(50, 440),
+            speed =  4)
+        self.state = state.MOVING_TO_DANCE
+
+    def move_to_talk(self)
+    
+
+    def fun_update(self):
+        """
+        Adjust fun level based on activity and volume
+        """
+        if self.state == PartyState.DANCING:
+            if volume < 7:  # Music not loud enough
+                self.fun -= 5
+            else:
+                self.fun += 1
+        elif self.state == PartyState.TALKING:
+            if volume > 3: # Music is too loud
+                self.fun -= 5
+            else:
+                self.fun += 1
+
+    def mood_update(self)
+        """
+        1 in 5 (?) chance that this partygoer wants to do something else
+        """
+        if randint(0,4) == 0:
+            if self.mood == Mood.DANCING:
+                self.mood == Mood.TALKING
+            elif self.mood == Mood.TALKING:
+                self.mood == Mood.DANCING        
+
     def draw(self):
         self.parent_surface.blit(self.surface, (self.x, self.y))
     
@@ -108,6 +236,7 @@ class Partygoer(object):
         self.mt_x = x
         self.mt_y = y
         self.move_speed = speed
+        self.reached_target = False        
 
     def update(self):
         """
@@ -115,21 +244,25 @@ class Partygoer(object):
         """ 
         self.move_towards_target()
     
+    """
+    def destroy(self):
+        global object_dict
+        (me for me in object_dict if me.id == self.id)
+    """    
+    
     def move_towards_target(self):
+        if self.mt_x == self.x and self.mt_y == self.y:
+            self.reached_target = True
         loc = pygame.math.Vector2(self.x, self.y)
         target = pygame.math.Vector2(self.mt_x, self.mt_y)
-        angle = loc.angle_to(target)
-        x_dist = self.move_speed * math.cos(math.radians(angle))
-        y_dist = self.move_speed * math.sin(math.radians(angle))
-        if self.mt_x > self.x:
-            self.x += x_dist
-        elif self.mt_x < self.x:
-            self.x -= x_dist
-        if self.mt_y > self.y:
-            self.y += y_dist
-        elif self.mt_y < y_dist:
-            self.y -= y_dist
-        
+        distance = loc.distance_to(target)
+        proportion = self.move_speed/distance
+        try:
+            new_pos = loc.lerp(target, proportion)
+            self.x = new_pos.x
+            self.y = new_pos.y
+        except ValueError:
+            return
 
 def gen_random_color():
     return  pygame.Color(
