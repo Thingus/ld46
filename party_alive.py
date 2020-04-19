58f64e8a5e7510c3cc232b8ceea0327383bc4fff
Thingus <fletcherroberts049@gmail.com>
2020-04-19 20:45:11 +0100

    Sound and renaming

--- party_alive.py
+++ party_alive.py
@@ -34,6 +34,7 @@ class PygView(object):
         running = True
         global object_dict
         global volume
+        sound = Soundtrack()
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -51,8 +52,8 @@ class PygView(object):
                             volume -= 1
             milliseconds = self.clock.tick(self.fps)
             self.playtime += milliseconds /1000.0
-            #self.draw_text("FPS: {:6.3}  PLAYTIME: {:6.3} SECONDS".format(
-            #               self.clock.get_fps(), self.playtime))
+            self.draw_text("FPS: {:6.3}  PLAYTIME: {:6.3} SECONDS".format(
+                           self.clock.get_fps(), self.playtime))
             for updateable in object_dict.values():
                 if "update" in dir(updateable):
                     updateable.update()
@@ -67,6 +68,31 @@ class PygView(object):
         fw, fh = self.font.size(text)
         surface = self.font.render(text, True, (0, 255, 0))
         self.screen.blit(surface, (0, 0))
+
+"""
+class Dancefloor(object):
+    def __init__(self):
+"""
+
+class Soundtrack(object):
+    def __init__(self):
+        self.doof_sample = pygame.mixer.Sound("doof_rough.ogg")
+        self.babble_sample = pygame.mixer.Sound("babble_rough.ogg")
+        self.doof = pygame.mixer.Channel(0)
+        self.babble = pygame.mixer.Channel(1)
+        vol_prop = 300/volume + 0.1    # avoid div by 0 errors
+        self.doof.set_volume(vol_prop)
+        self.babble.set_volume(1-vol_prop)
+        self.doof.play(self.doof_sample, -1, fade_ms = 100)
+        self.babble.play(self.babble_sample, -1, fade_ms = 100)
+        self.id = 3
+        global object_dict
+        object_dict[self.id] = self
+
+    def update(self):
+        vol_prop = (volume + 0.001)/10
+        self.doof.set_volume(vol_prop)
+        self.babble.set_volume(1-vol_prop)
 
 
 class VolumeSlider(object):
@@ -177,6 +203,9 @@ class PartyState(Enum):
 
 
 class Partygoer(object):
+
+    def idle_animation(self):
+        pass
     
     def __init__(self, parent_surface, x, y,  width = 40, height=120, mood=None):
         self.width = width
@@ -186,7 +215,7 @@ class Partygoer(object):
         self.x = x
         self.y = y
         self.generate_sprite()
-        self.id = randint(100, 300)
+        self.id = randint(100, 299)
         if not mood:
             self.mood = Mood(randint(1,2))
         self.state = PartyState.ENTERING
@@ -195,6 +224,8 @@ class Partygoer(object):
         self.reached_target = True
         self.mt_x = x
         self.mt_y = y
+        self.animation_frame = 0
+        self.animation = self.idle_animation
         global object_dict
         object_dict[self.id] = self
 
@@ -252,10 +283,10 @@ class Partygoer(object):
                 self.move_to_talk()
         elif self.state == PartyState.MOVING_TO_DANCE:
             if self.reached_target:
-                self.state = PartyState.DANCING
+                self.start_dancing()
         elif self.state == PartyState.MOVING_TO_TALK:
             if self.reached_target:
-                self.state = PartyState.TALKING
+                self.start_talking()
         elif self.state == PartyState.MOVING_TO_LEAVE:
             if self.reached_target:
                 self.destroy()
@@ -276,6 +307,7 @@ class Partygoer(object):
             x = randint(10, 300),
             y = randint(50, 440),
             speed =  4)
+        self.animation = self.idle_animation
         self.state = PartyState.MOVING_TO_DANCE
 
     def move_to_talk(self):
@@ -284,6 +316,7 @@ class Partygoer(object):
             x = randint(350, 620),
             y = randint(50, 440),
             speed = 2)
+        self.animation = self.idle_animation
         self.state = PartyState.MOVING_TO_TALK
 
     def move_to_leave(self):
@@ -291,8 +324,18 @@ class Partygoer(object):
         self.set_move_target(
             x = randint(200, 400),
             y = randint(450, 480),
-            speed = 2) 
-        self.state = PartyState.MOVING_TO_LEAVE        
+            speed = 2)
+        self.animation = self.idle_animation
+        self.state = PartyState.MOVING_TO_LEAVE
+
+    def start_dancing(self):
+        # Set the animation and state to dancing
+        self.animation = self.dance_animation
+        self.state = PartyState.DANCING
+
+    def start_talking(self):
+        self.animation = self.talk_animation
+        self.state = PartyState.TALKING
 
     def fun_update(self):
         """
@@ -311,7 +354,7 @@ class Partygoer(object):
 
     def mood_update(self):
         """
-        1 in 5 (?) chance that this partygoer wants to do something else
+        1 in 10 (?) chance that this partygoer wants to do something else
         """
         if randint(0, 10) == 0:
             if self.mood == Mood.DANCING:
@@ -320,6 +363,33 @@ class Partygoer(object):
                 self.mood = Mood.DANCING
         if self.fun <= 0:
             self.mood = Mood.LEAVING
+
+    def dance_animation(self):
+        """
+        30 frame animation of a bounce
+        """
+        self.animation_frame += 1
+        if self.animation_frame <= 6:
+            self.y -= 2
+        elif self.animation_frame > 6 and self.animation_frame <= 12:
+            self.y += 2
+        elif self.animation_frame > 18 and self.animation_frame <= 29:
+            pass
+        else:
+            self.animation_frame = 0
+    
+    def talk_animation(self):
+        """
+        60 frame animation of a nice chat
+        """
+        self.animation_frame += 1
+        if self.animation_frame <= 10:
+            if self.animation_frame % 2 == 0:
+                self.x -= 2
+            else:
+                self.x += 2
+        elif self.animation_frame >= 60:
+            self.animation_frame = 0
 
     def draw(self):
         self.parent_surface.blit(self.surface, (self.x, self.y))
@@ -339,8 +409,9 @@ class Partygoer(object):
             self.ticker = 0
             self.fun_update()
             self.mood_update()
-            self.state_update() 
+        self.state_update() 
         self.move_towards_target()
+        self.animation()
     
     def destroy(self):
         global object_dict
